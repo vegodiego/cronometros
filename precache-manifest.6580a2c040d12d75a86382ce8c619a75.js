4bc7bd72edb3afa1871864111619e12727ea02da
vegodiego <53877309+vegodiego@users.noreply.github.com>
2020-04-27 23:09:04 -0500

    Updates

---
+++ precache-manifest.6580a2c040d12d75a86382ce8c619a75.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac65367e5a590ff23fe1b4d6251563e3",
+    "url": "/cronometros/index.html"
+  },
+  {
+    "revision": "dbeb93c6d9b306e01592",
+    "url": "/cronometros/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "8ca9b5b1ed652b690a0f",
+    "url": "/cronometros/static/css/main.ba323c99.chunk.css"
+  },
+  {
+    "revision": "dbeb93c6d9b306e01592",
+    "url": "/cronometros/static/js/2.2330b87d.chunk.js"
+  },
+  {
+    "revision": "86493497955f2c03ad4534f44ae20baa",
+    "url": "/cronometros/static/js/2.2330b87d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ca9b5b1ed652b690a0f",
+    "url": "/cronometros/static/js/main.3b09d654.chunk.js"
+  },
+  {
+    "revision": "393ce55902f23a3ba753",
+    "url": "/cronometros/static/js/runtime-main.cf399b53.js"
+  }
+]);
